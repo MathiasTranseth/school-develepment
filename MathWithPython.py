e89297eb38286e5c2fa74a0df82ecfa3e678e0da
Mathias Transeth <mathiastranseth@outlook.com>
2021-08-24 04:16:43 -0700

    small math program

---
+++ MathWithPython.py
@@ -0,0 +1,7 @@
+# regner ut areal og omkrets
+høyde = int(input("skriv høyde "))
+bredde = int(input("skriv bredde "))
+areal = str(høyde * bredde)
+omkrets = str((høyde + bredde) * 2)
+print ("arealet er " + areal)
+print ("omkretsen er " + omkrets)
